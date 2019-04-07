79da451a37f003e9fd1b1b8c8cd5f8fa75d4e908
tjha <tjha@umich.edu>
2019-04-07 19:30:37 -0400

    modified tabs to spaces for consistancy across envirnments

--- services/review_services.go
+++ services/review_services.go
@@ -1,26 +1,26 @@
 package services
 
 import (
-	"class-review-backend/repositories"
+    "class-review-backend/repositories"
 
-	"class-review-backend/models"
+    "class-review-backend/models"
 )
 
 type IReviewService interface {
-	GetReview(uint64) (*models.Review, error)
+    GetReview(uint64) (*models.Review, error)
 }
 
 // Implements IReviewService
 type ReviewService struct {
-	Repositories *repositories.Repositories
+    Repositories *repositories.Repositories
 }
 
 func DefaultReviewServices(repos *repositories.Repositories) *ReviewService {
-	return &ReviewService{
-		Repositories: repos,
-	}
+    return &ReviewService{
+        Repositories: repos,
+    }
 }
 
 func (rs *ReviewService) GetReview(id uint64) (*models.Review, error) {
-	return rs.Repositories.ReviewRepository.GetReview(id)
+    return rs.Repositories.ReviewRepository.GetReview(id)
 }
