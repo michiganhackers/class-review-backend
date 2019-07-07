5624733d6384fe2a64e7321f82ec46bcbbcd2f3f
christopherhafke <chafke@umich.edu>
2019-07-07 14:58:37 -0700

    Beginning to work on middleware

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -12,7 +12,28 @@ type Controllers struct {
 	ProfessorController *ProfessorController
 }
 
+type Routes struct {
+	Private *gin.RouterGroup
+	Public  *gin.RouterGroup
+	Admin   *gin.RouterGroup
+}
+
+func AuthenticationRequired(auths ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO
+	}
+}
+
 func DefaultControllers(r *gin.Engine, services *services.Services) *Controllers {
+
+	routes := &Routes{
+		Private: r.Group("/"),
+		Public:  r.Group("/"),
+		Admin:   r.Group("/admin"),
+	}
+
+	routes.Private.Use(AuthenticationRequired("private", "admin"))
+
 	controllers := &Controllers{
 		ReviewController:    DefaultReviewController(r, services),
 		CourseController:    DefaultCourseController(r, services),
